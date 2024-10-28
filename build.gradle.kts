aced7d4fcabd544c3c2a5ca7ec6b0d920cbeeb5f
rojat938 <65051747+rojat938@users.noreply.github.com>
2024-10-28 14:47:49 +0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+
+        val  nav_version = "2.8.1"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
+}
